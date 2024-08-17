2b3935eb8e339a8a6649733028f8e5c8b2c86f0c
AbuMaraqa <maraqamohamad@outlook.sa>
2024-08-17 09:37:46 +0300

    asd

--- resources/views/admin/accounting/orders_sales/ajax/product_list_ajax.blade.php
+++ resources/views/admin/accounting/orders_sales/ajax/product_list_ajax.blade.php
@@ -1,6 +1,7 @@
 <table class="table table-sm table-bordered table-hover">
     <thead>
         <tr>
+            <th>الباركودس</th>
             <th>الصنف</th>
             <th>لصنف</th>
         </tr>
@@ -8,11 +9,12 @@
     <tbody>
         @if($data->isEmpty())
             <tr>
-                <td colspan="2" class="text-center">لا يوجد بيانات</td>
+                <td colspan="3" class="text-center">لا يوجد بيانات</td>
             </tr>
         @else
             @foreach($data as $key)
                 <tr>
+                    <td>{{ $key->barcode }}</td>
                     <td>{{ $key->product_name_ar }}</td>
                     <td>
                         <button class="btn btn-success btn-sm" onclick="create_orders_sales_items({{ $key->id }})"><span class="fa fa-plus"></span></button>
