5b404140c4d3315fe92590fd7f701a6506c1889c
AbuMaraqa <maraqamohamad@outlook.sa>
2024-10-28 09:18:28 +0200

    asdasd

--- resources/views/admin/accounting/bonds/payment_bond/modals/update_check_payment_type.blade.php
+++ resources/views/admin/accounting/bonds/payment_bond/modals/update_check_payment_type.blade.php
@@ -1,54 +1,57 @@
 <div class="modal fade" id="update_check_payment_type_modal">
     <div class="modal-dialog modal-lg">
-        <form action="{{ route('accounting.purchase_invoices.create_purchase_invoices_from_order') }}" method="post" enctype="multipart/form-data">
+        <form action="{{ route('bonds.update_check_information') }}" id="check_create_form" method="post"
+            enctype="multipart/form-data">
             @csrf
-            <input type="hidden" name="order_id" id="order_input">
-            <input type="hidden" name="supplier_user_id" id="supplier_input">
-            <input type="hidden" name="invoice_type" value="sales">
+            <input type="hidden" name="bonds_id" id="bonds_id">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title">فاتورة من عرض سعر</h4>
+                    <h4 class="modal-title">معلومات الشيك</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
                     <div class="row">
-                        <div class="col-md-8">
+                        <div class="col-md-6">
                             <div class="form-group">
-                                <label for="">بحث عن عرض سعر</label>
-                                <input onkeyup="search_order_ajax()" id="input_search" type="text" class="form-control" placeholder="ابحث عن طلبية">
-                                {{-- <select class="form-control select2bs4" name="order_id" id="">
-                                    <option value="">اختر الطلبية</option>
-                                    @foreach ($order as $key)
-                                        <option value="{{ $key->id }}">{{ $key->reference_number }}</option>
-                                    @endforeach
-                                </select> --}}
+                                <label for="">رقم الشيك</label>
+                                <input type="text" name="check_number" id="check_number_edit" class="form-control"
+                                    placeholder="رقم الشيك">
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-6">
                             <div class="form-group">
-                                <label for="">المورد</label>
-                                <select class="form-control select2bs4" name="supplier_id" onchange="search_order_ajax()" id="supplier_id">
-                                    <option value="">اختر المورد</option>
-                                    @foreach ($users as $key)
-                                        <option value="{{ $key->id }}">{{ $key->name }}</option>
-                                    @endforeach
+                                <label for="">تاريخ الاستحقاق</label>
+                                <input type="date" name="due_date" id="due_date_edit" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="">اسم البنك</label>
+                                <input name="bank_name" id="bank_name_edit" type="text" class="form-control"
+                                    placeholder="اسم البنك">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="">حالة الشيك</label>
+                                <select name="check_status" class="form-control" id="check_status">
+                                    <option value="paid">مصروف</option>
+                                    <option value="under_collection">في التحصيل</option>
+                                    <option value="returned">راجع</option>
+                                    <option value="portfolio">في المحفظة</option>
                                 </select>
                             </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12" id="search_order">
-
                         </div>
                     </div>
                 </div>
                 <div class="modal-footer justify-content-between">
                     <button type="button" class="btn btn-danger" data-dismiss="modal">اغلاق</button>
-                    {{-- <button type="submit" class="btn btn-primary">حفظ</button> --}}
+                    <button type="submit" class="btn btn-dark">حفظ</button>
                 </div>
             </div>
         </form>
+
     </div>
 </div>
