8bcf368716f249a322c192683a12deec4245843d
Mohamad Maraqa <mohamadmaraqa@Mohamads-MacBook-Pro.local>
2024-12-12 11:19:55 +0200

    masd

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'mohamad';
     if (\Illuminate\Support\Facades\Auth::check()) {
         return redirect()->route('home');
     } else {
